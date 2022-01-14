5b8881f97f93855e1e6dde0db39d9d8ee6cfba7f
Mohamed Alaa <mohamedalaashafik98@gmail.com>
2022-01-14 20:40:18 +0200

    TIMER DRIVER V1.2

--- TIMERwithInterrupt/main.c
+++ TIMERwithInterrupt/main.c
@@ -7,8 +7,8 @@
 void ISR0(void);
 void ISR2(void);
 
-uint8_t gu8_counter=0;//Declaration of global counter to count interrupts happening
-uint8_t gu8_flag=0;//Declaration of global flag
+uint8_t gu8_counter1=0,gu8_counter2=0;//Declaration of global counter to count interrupts happening
+uint8_t gu8_flag1=0,gu8_flag2=0;//Declaration of global flag
 int main(void)
 {
 	DIO_SETpinDir(DIO_PORTB,DIO_PIN4,DIO_OUTPUT);//Set the direction of pin to be output
@@ -18,9 +18,9 @@ int main(void)
 	TIMER2_SETcompareMatchValue(125);
 	TIMER_START(2);//Start timer
 	TIMER2_callback(&ISR2);//call timer callback
-	//TimerInit(0);
-	//TIMER_START(0);
-	//TIMER0_callback(&ISR0);
+	TimerInit(0);
+	TIMER_START(0);
+	TIMER0_callback(&ISR0);
 	while(1)
 	{
 
@@ -31,24 +31,24 @@ int main(void)
 void ISR0(void)
 {
 	/*check if the led on or off*/
-	if(gu8_flag==0)
+	if(gu8_flag1==0)
 	{
-		gu8_counter++;
-		if(gu8_counter==16)
+		gu8_counter1++;
+		if(gu8_counter1==16)
 		{
 			DIO_SETpinVal(DIO_PORTB,DIO_PIN5,DIO_HIGH);//LED on
-			gu8_counter=0;
-			gu8_flag=1;
+			gu8_counter1=0;
+			gu8_flag1=1;
 		}
 	}
 	else
 	{
-		gu8_counter++;
-		if(gu8_counter==10)
+		gu8_counter1++;
+		if(gu8_counter1==10)
 		{
 			DIO_SETpinVal(DIO_PORTB,DIO_PIN5,DIO_LOW);//LED off
-			gu8_counter=0;
-			gu8_flag=0;
+			gu8_counter1=0;
+			gu8_flag1=0;
 		}
 	}
 }
@@ -57,24 +57,24 @@ void ISR0(void)
 void ISR2(void)
 {
 	/*check if the led on or off*/
-	if(gu8_flag==0)
+	if(gu8_flag2==0)
 	{
-		gu8_counter++;
-		if(gu8_counter==125)
+		gu8_counter2++;
+		if(gu8_counter2==125)
 		{
 			DIO_SETpinVal(DIO_PORTB,DIO_PIN4,DIO_HIGH);//LED on
-			gu8_counter=0;
-			gu8_flag=1;
+			gu8_counter2=0;
+			gu8_flag2=1;
 		}
 	}
 	else
 	{
-		gu8_counter++;
-		if(gu8_counter==75)
+		gu8_counter2++;
+		if(gu8_counter2==75)
 		{
 			DIO_SETpinVal(DIO_PORTB,DIO_PIN4,DIO_LOW);//LED off
-			gu8_counter=0;
-			gu8_flag=0;
+			gu8_counter2=0;
+			gu8_flag2=0;
 		}
 	}
 }
